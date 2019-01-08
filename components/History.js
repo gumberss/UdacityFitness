1df4e227e159a3b7091d27cb44ff0daf77ac0670
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-01-08 18:49:00 -0200

    feat: Add udaciFitness-calendar component

--- components/History.js
+++ components/History.js
@@ -6,31 +6,53 @@ import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDetailsReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
 
 class History extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         const { dispatch } = this.props
 
         fetchCalendarResults()
-        .then(entries => dispatch(receiveEntries(entries)))
-        .then(({ entries }) => {
-            !entries[timeToString()] && dispatch(addEntry({
-                [timeToString()]: getDetailsReminderValue()
-            }))
-        })
+            .then(entries => dispatch(receiveEntries(entries)))
+            .then(({ entries }) => {
+                !entries[timeToString()] && dispatch(addEntry({
+                    [timeToString()]: getDetailsReminderValue()
+                }))
+            })
     }
 
-    render() {
+    renderItem = ({ today, ...metrics }, formattedDate, key) => {
+        return (<View id={JSON.stringify(today)}>
+            {today
+                ? <Text>{JSON.stringify(today)}</Text>
+                : <Text>{JSON.stringify(metrics)}</Text>
+
+            }
+        </View>)
+    }
+    renderEmptyDate = (formattedDate) => {
         return (
             <View>
-                <Text>{JSON.stringify(this.props)}</Text>
+                <Text>No Data for this day</Text>
             </View>
+        )
+    }
+    render() {
+
+        const { entries } = this.props
+
+        return (
+            <UdaciFitnessCalendar
+                items={entries}
+                renderItem={this.renderItem}
+                renderEmptyDate={this.renderEmptyDate}
+            />
         );
     }
 }
 
-const mapStateToProps  = entries => {
+const mapStateToProps = entries => {
     return {
         entries
     }
