a511f4ad9b4686f480c7c24ef4053c542bf2f0e5
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-01-09 14:52:17 -0200

    feat: Creation layout of EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import MetricCard from './MetricCard'
 
-export default class EntryDetail extends Component {
+class EntryDetail extends Component {
 
     static navigationOptions = ({ navigation }) => {
         const { entryId } = navigation.state.params
@@ -15,11 +18,35 @@ export default class EntryDetail extends Component {
             title: `${month}/${day}/${year}`
         }
     }
+
     render() {
+
+        const { metrics } = this.props
+
         return (
-            <View>
+            <View style={styles.container}>
+                <MetricCard metrics={metrics} />
                 <Text>Entry Detail - {this.props.navigation.state.params.entryId} </Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15
+    }
+})
+
+const mapStateToProps = (state, { navigation }) => {
+    const { entryId } = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId]
+    }
+}
+
+export default connect(mapStateToProps)(EntryDetail)
