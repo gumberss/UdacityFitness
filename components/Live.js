0feabb8bf267fb3773504fa779071966bcc876a6
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-01-10 11:59:10 -0200

    feat: Add values to spee, altitude and direction dynamic

--- components/Live.js
+++ components/Live.js
@@ -8,7 +8,7 @@ import { calculateDirection } from '../utils/helpers'
 export default class Live extends Component {
     state = {
         coords: null,
-        status: 'granted',
+        status: null,
         direction: ''
     }
 
@@ -88,7 +88,7 @@ export default class Live extends Component {
             <View style={styles.container}>
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
-                    <Text style={styles.direction} >North</Text>
+                    <Text style={styles.direction}>{ direction }</Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
@@ -96,7 +96,7 @@ export default class Live extends Component {
                             Altitude
                         </Text>
                         <Text style={[styles.subHeader, { color: white }]} >
-                            {200} Feet
+                            {Math.round(coords.altitude * 3.2808)} Feet
                         </Text>
                     </View>
 
@@ -105,7 +105,7 @@ export default class Live extends Component {
                             Speed
                         </Text>
                         <Text style={[styles.subHeader, { color: white }]} >
-                            {300} MPH
+                            {(coords.speed * 2.2369).toFixed(1)} MPH
                         </Text>
                     </View>
                 </View>
